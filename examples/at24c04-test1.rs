a62238abd544afe84668643fc327a5d736573bc6
Sergey Matyukevich <geomatsi@gmail.com>
2018-12-25 00:29:01 +0300

    at24x: convert example into series of tests
    Run examples as tests:
    $ cargo test --example=at24c04-test1 -- --test-threads=1
    
    Note:
    This example is currently not compatible with stock eeprom24x crate
    due to addressing issue for low memory ( <= at24c16) devices.
    It is compatible with hacked eeprom24x version which uses
    single byte for memory address.
    
    TODO: update example after eeprom24x is fixed.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- examples/at24c04-test1.rs
+++ examples/at24c04-test1.rs
@@ -1,53 +1,77 @@
 extern crate ftdi_embedded_hal as hal;
-use crate::hal::ft232h::FT232H;
-
 extern crate eeprom24x;
-use eeprom24x::Eeprom24x;
-use eeprom24x::SlaveAddr;
-
-use std::thread::sleep;
-use std::time::Duration;
 
 fn main() {
-    let dev = FT232H::init(0x0403, 0x6014).unwrap();
-    let i2c = dev.i2c().unwrap();
-    let mut eeprom = Eeprom24x::new_24x04(i2c, SlaveAddr::default());
+    println!("Run example tests: cargo test --example=at24c04-test1 -- --test-threads=1");
+}
 
-    let five_ms = Duration::from_millis(100);
-    let addr = [0x1, 0x0];
-    let d_out = 0xe5;
-    eeprom.write_byte(addr, d_out).unwrap();
-    sleep(five_ms);
-    let d_in = eeprom.read_byte(addr).unwrap();
-    println!("out 0x{:x} in 0x{:x}", d_out, d_in);
+#[cfg(test)]
+mod test {
+    use crate::hal::ft232h::FT232H;
 
-    //let delay = Duration::from_millis(100);
-    //let data = eeprom.read_byte([0x0, 0x0]).unwrap();
-    //println!("data 0x{:x}", data);
-    //sleep(delay);
-    //let data = eeprom.read_byte([0x1, 0x0]).unwrap();
-    //println!("data 0x{:x}", data);
-    //sleep(delay);
-    //let data = eeprom.read_byte([0x2, 0x0]).unwrap();
-    //println!("data 0x{:x}", data);
-    //sleep(delay);
-    //let data = eeprom.read_byte([0x3, 0x0]).unwrap();
-    //println!("data 0x{:x}", data);
-    //sleep(delay);
+    use eeprom24x::Eeprom24x;
+    use eeprom24x::SlaveAddr;
 
-    //let addr = [0x0, 0x0];
-    //let mut data = [0; 4];
-    //eeprom.read_data(addr, &mut data).unwrap();
-    //println!("data 0x{:x} 0x{:x} 0x{:x} 0x{:x}", data[0], data[1], data[2], data[3]);
+    use std::thread::sleep;
+    use std::time::Duration;
 
-    //let delay = Duration::from_millis(100);
-    //eeprom.write_byte([0x0, 0x0], 0xaa).unwrap();
-    //sleep(delay);
-    //eeprom.write_byte([0x1, 0x0], 0xbb).unwrap();
-    //sleep(delay);
-    //eeprom.write_byte([0x2, 0x0], 0xcc).unwrap();
-    //sleep(delay);
-    //eeprom.write_byte([0x3, 0x0], 0xdd).unwrap();
-    //sleep(delay);
+    #[test]
+    fn at24x_test_t1() {
+        let dev = FT232H::init(0x0403, 0x6014).unwrap();
+        let i2c = dev.i2c().unwrap();
+        let mut eeprom = Eeprom24x::new_24x04(i2c, SlaveAddr::default());
 
+        let delay = Duration::from_millis(5);
+        let byte_w = 0xe5;
+        let addr = 0x0;
+
+        eeprom.write_byte(addr, byte_w).unwrap();
+        sleep(delay);
+
+        let byte_r = eeprom.read_byte(addr).unwrap();
+
+        assert_eq!(byte_w, byte_r);
+    }
+
+    #[test]
+    fn at24x_test_t2() {
+        let dev = FT232H::init(0x0403, 0x6014).unwrap();
+        let i2c = dev.i2c().unwrap();
+        let mut eeprom = Eeprom24x::new_24x04(i2c, SlaveAddr::default());
+
+        let delay = Duration::from_millis(5);
+        let data_w: [u8; 4] = [0xaa, 0xbb, 0xcc, 0xdd];
+        let mut data_r: [u8; 4] = [0; 4];
+
+        for i in 0..data_w.len() {
+            eeprom.write_byte(i as u8, data_w[i]).unwrap();
+            sleep(delay);
+        }
+
+        for i in 0..data_r.len() {
+            data_r[i] = eeprom.read_byte(i as u8).unwrap();
+        }
+
+        assert_eq!(data_w, data_r);
+    }
+
+    #[test]
+    fn at24x_test_t3() {
+        let dev = FT232H::init(0x0403, 0x6014).unwrap();
+        let i2c = dev.i2c().unwrap();
+        let mut eeprom = Eeprom24x::new_24x04(i2c, SlaveAddr::default());
+
+        let delay = Duration::from_millis(5);
+        let data_w: [u8; 4] = [0xaa, 0xbb, 0xcc, 0xdd];
+        let mut data_r: [u8; 4] = [0; 4];
+
+        for i in 0..data_w.len() {
+            eeprom.write_byte(i as u8, data_w[i]).unwrap();
+            sleep(delay);
+        }
+
+        eeprom.read_data(0x0, &mut data_r).unwrap();
+
+        assert_eq!(data_w, data_r);
+    }
 }
