f98d2caded7e0e783036c421d3414c90d4fb678f
Sergey Matyukevich <geomatsi@gmail.com>
2018-12-27 23:41:11 +0300

    i2c example: lm75 temperature sensor
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ examples/lm75-test1.rs
@@ -0,0 +1,22 @@
+extern crate ftdi_embedded_hal as hal;
+extern crate lm75;
+
+use crate::hal::ft232h::FT232H;
+
+use lm75::{Lm75, SlaveAddr};
+
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    let dev = FT232H::init(0x0403, 0x6014).unwrap();
+    let i2c = dev.i2c().unwrap();
+    let mut sensor = Lm75::new(i2c, SlaveAddr::default());
+    let delay = Duration::from_secs(1);
+
+    for _ in 0..5 {
+        let temperature = sensor.read_temperature().unwrap();
+        println!("Temperature: {}", temperature);
+        sleep(delay);
+    }
+}
