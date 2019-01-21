0eac7743b2cc3c18722bf6634c49fd3ba2f2780d
Sergey Matyukevich <geomatsi@gmail.com>
2019-01-21 22:57:28 +0300

    eeprom24x: update eeprom24x deps and add one more test
    Bump eeprom24x driver to 0.3.0 version which includes the fix
    for low memory devices.
    
    Add at24c04 test: test cases when high bytes of address become
    a part of i2c address.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- examples/at24c04-test1.rs
+++ examples/at24c04-test1.rs
@@ -44,12 +44,12 @@ mod test {
         let mut data_r: [u8; 4] = [0; 4];
 
         for i in 0..data_w.len() {
-            eeprom.write_byte(i as u8, data_w[i]).unwrap();
+            eeprom.write_byte(i as u32, data_w[i]).unwrap();
             sleep(delay);
         }
 
         for i in 0..data_r.len() {
-            data_r[i] = eeprom.read_byte(i as u8).unwrap();
+            data_r[i] = eeprom.read_byte(i as u32).unwrap();
         }
 
         assert_eq!(data_w, data_r);
@@ -66,7 +66,7 @@ mod test {
         let mut data_r: [u8; 4] = [0; 4];
 
         for i in 0..data_w.len() {
-            eeprom.write_byte(i as u8, data_w[i]).unwrap();
+            eeprom.write_byte(i as u32, data_w[i]).unwrap();
             sleep(delay);
         }
 
@@ -82,7 +82,7 @@ mod test {
         let mut eeprom = Eeprom24x::new_24x04(i2c, SlaveAddr::default());
 
         let delay = Duration::from_millis(50);
-        let addrs: [u8; 4] = [0x00, 0x10, 0x20, 0x30];
+        let addrs: [u32; 4] = [0x00, 0x10, 0x20, 0x30];
         let mut data_r = [0x00; 16];
         let data_w = [0xAB; 16];
 
@@ -91,6 +91,46 @@ mod test {
             sleep(delay);
             eeprom.read_data(*addr, &mut data_r).unwrap();
             assert_eq!(data_w, data_r);
+        }
+    }
+
+    #[test]
+    fn at24x_test_t5() {
+        let dev = FTx232H::init(0x0403, 0x6014).unwrap();
+        let i2c = dev.i2c(hal::i2c::I2cSpeed::CLK_400kHz).unwrap();
+        let mut eeprom = Eeprom24x::new_24x04(i2c, SlaveAddr::default());
+        let delay = Duration::from_millis(5);
+
+        // check high memory addresses: 1 bit passed as a part of i2c addr
+        let addrs1: [u32; 4] = [0x100, 0x10F, 0x1F0, 0x1EE];
+        let byte_w1 = 0xe5;
+        let addrs2: [u32; 4] = [0x00, 0x0F, 0xF0, 0xEE];
+        let byte_w2 = 0xaa;
+
+        // write bytes
+
+        for addr in addrs1.iter() {
+            eeprom.write_byte(*addr, byte_w1).unwrap();
+            sleep(delay);
+        }
+
+        for addr in addrs2.iter() {
+            eeprom.write_byte(*addr, byte_w2).unwrap();
+            sleep(delay);
+        }
+
+        // read bytes and check
+
+        for addr in addrs1.iter() {
+            let byte_r = eeprom.read_byte(*addr).unwrap();
+            assert_eq!(byte_w1, byte_r);
+            sleep(delay);
+        }
+
+        for addr in addrs2.iter() {
+            let byte_r = eeprom.read_byte(*addr).unwrap();
+            assert_eq!(byte_w2, byte_r);
+            sleep(delay);
         }
     }
 }
